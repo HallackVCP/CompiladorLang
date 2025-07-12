6649b7d71663c52b9357d6a07fba307ca5e082d0
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-07-11 23:08:40 -0300

    remove comment

--- src/main/java/org/example/lang/ast/cmd/ReturnCmd.java
+++ src/main/java/org/example/lang/ast/cmd/ReturnCmd.java
@@ -15,7 +15,6 @@ import java.util.List;
 /**
  * Representa o comando 'return', que define os valores de retorno de uma função.
  * É seguido por uma lista de expressões separadas por vírgula.
- * (Esta implementação foi simplificada para um único valor de retorno para corresponder ao parser do exemplo.)
  */
 public record ReturnCmd(List<Exp> exps) implements Cmd{
     @Override
