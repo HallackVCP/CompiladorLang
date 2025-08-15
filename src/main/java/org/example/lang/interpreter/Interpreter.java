1a4192555541fde48e9e54f4f6467180792e5a8e
vinicius <viniciushallack@gmail.com>
2025-08-15 19:33:58 -0300

    FIX: trocando println por print

--- src/main/java/org/example/lang/interpreter/Interpreter.java
+++ src/main/java/org/example/lang/interpreter/Interpreter.java
@@ -208,7 +208,7 @@ public class Interpreter implements Visitor<Value> {
 
     @Override
     public Value visit(PrintCmd c) {
-        System.out.println(c.exp().accept(this).toString());
+        System.out.print(c.exp().accept(this).toString());
         return new VoidValue();
     }
 
